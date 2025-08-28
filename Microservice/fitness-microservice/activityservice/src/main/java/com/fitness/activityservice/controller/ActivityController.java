40eee1ccffc7a635d2894eb13ded5df5c1616dd6
4heisenberG4 <139671544+4heisenberG4@users.noreply.github.com>
2025-08-28 19:00:08 +0530

    feat: add MongoDB configuration and update ActivityController to use RequestBody

--- Microservice/fitness-microservice/activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ Microservice/fitness-microservice/activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class ActivityController {
 
 
     @PostMapping
-    public ResponseEntity<ActivityResponse> trackActivity(ActivityRequest request) {
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
         // Logic to track activity
         return ResponseEntity.ok(activityService.trackActivity(request));
 

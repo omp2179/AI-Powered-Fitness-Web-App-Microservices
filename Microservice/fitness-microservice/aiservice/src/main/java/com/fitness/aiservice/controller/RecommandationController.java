43c1b0ec2ece51f8cc2e3f3d276a1cc79a1a1c22
omp2179 <202301163@dau.ac.in>
2025-09-20 23:07:24 +0530

    feat: add MongoDB configuration and recommendation model with controller

---
+++ Microservice/fitness-microservice/aiservice/src/main/java/com/fitness/aiservice/controller/RecommandationController.java
@@ -0,0 +1,12 @@
+package com.fitness.aiservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendations")
+public class RecommandationController {
+
+}
